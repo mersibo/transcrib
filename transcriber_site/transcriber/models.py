f96ac5a88b453894fabdca5425692c6624eefcd2
George <gaataev@edu.hse.ru>
2024-07-24 22:35:48 +0300

    Update models.py

--- transcriber_site/transcriber/models.py
+++ transcriber_site/transcriber/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Transcription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    original_file = models.CharField(max_length=255)
+    transcribed_file = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.original_file}"
