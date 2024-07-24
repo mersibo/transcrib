70ae4810c37e019ffb14cb9f3d38d77b88497cb9
Ilya <peragov04@mail.com>
2024-07-24 12:19:47 +0300

    add design

--- transcriber_site/transcriber/views.py
+++ transcriber_site/transcriber/views.py
@@ -18,6 +18,10 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 return redirect('index')
+            else:
+                return render(request, 'login.html', {'form': form, 'error': 'Неверное имя пользователя или пароль'})
+        else:
+            return render(request, 'login.html', {'form': form})
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
